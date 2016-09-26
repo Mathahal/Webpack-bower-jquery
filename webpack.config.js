f282e6bd8d24e622484bed4405ed32990093e53a
iso-masaharu <iso-masaharu@SYS-Ebisu-0028.local>
2016-09-26 18:47:20 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack'),
+    path = require('path')
+
+module.exports = {
+  entry: {
+    entry: path.join( __dirname, './entry.js' )
+  },
+  output: {
+    path: __dirname,
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.sass$/,
+        exclude: /(application|node_modules|bootstrap|public_html|.git)/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /(application|node_modules|bootstrap|public_html|.git)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    root: [ path.join(__dirname, 'bower_components') ]
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    )
+  ]
+}
